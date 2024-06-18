3a2ae98878c77cd6d52b07c0076db6f033ca60c5
Ren8602 <ibragimovren99@gmail.com>
2024-06-18 16:53:51 +0500

    first commit

---
+++ import sys.py
@@ -0,0 +1,18 @@
+import sys
+left = int(input())
+right = int(input())
+count = 0
+a = 0
+n = sys.maxsize
+for i in range(0, n):
+    a = input()
+    if a.isdigit():
+        if int(a) not in range(left, right+1):
+            count = count + 1
+            break
+    else:
+        break
+if count == 0:
+    print('TRUE')
+else:
+    print('FALSE')
