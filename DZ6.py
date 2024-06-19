b6df3da8f23bfbf14f0f602e90288e3d60dfbc55
Ren8602 <ibragimovren99@gmail.com>
2024-06-19 16:42:47 +0500

    DZ6 completed

---
+++ DZ6.py
@@ -0,0 +1,9 @@
+a = input()
+count = 0
+
+for i in a:
+    if  (i == '!' or  i == '%' or i == '#' or i == '@') and i != ' ':
+        a = a.replace(i, '')
+        count = count + 1
+print(count)   
+print(a.lower())
