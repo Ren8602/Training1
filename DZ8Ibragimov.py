cda7760837b9ad6358945606677ef4c839edc24c
Ren8602 <ibragimovren99@gmail.com>
2024-06-24 15:17:10 +0500

    chanhes with try

--- DZ8Ibragimov.py
+++ DZ8Ibragimov.py
@@ -1,13 +1,16 @@
 k = int(input())
 b =[]
 i=0
-while n:= input():
-    i+=1
-    a = n.split()
-    spisok_int = list(map(int, a))
-    if i<=k:
-        maximum = max(spisok_int)
-        b.append(maximum)
+try:
+    while n:= input():
+        i+=1
+        a = n.split()
+        spisok_int = list(map(int, a))
+        if i<=k:
+            maximum = max(spisok_int)
+            b.append(maximum)
+except:
+    pass
 
 b.sort(reverse=True)
 result = ';'.join(str(i) for i in b)
