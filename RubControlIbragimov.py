3f3d01037a41781e6e36e0ae22310b4634bd2d6d
Ren8602 <ibragimovren99@gmail.com>
2024-06-28 15:15:30 +0500

    без второго цикла

--- RubControlIbragimov.py
+++ RubControlIbragimov.py
@@ -11,12 +11,8 @@ a = s.split()
 b = []
 for i in a:
     k = 0
-    if len(i) >= 5 and len(set(i)) >= 4:
-        for j in a:
-            if j == i:
-                k+=1
-            if k > 2:
-                b.append(i)
+    if len(i) >= 5 and len(set(i)) >= 4 and a.count(i) > 2:
+        b.append(i)
 
 result = sorted(list(set(b)))
 for i in result:
