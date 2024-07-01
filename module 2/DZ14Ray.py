638f82be6c674a9550d6164ef7b54581892f237a
Rail Rafikov <rayraf@mail.ru>
2024-07-02 00:00:35 +0500

    Try to resolve 14th HW

---
+++ module 2/DZ14Ray.py
@@ -0,0 +1,6 @@
+for i in map(lambda x: x * 2 if x % 2 == 0 else -x , l := list(map(int, input().split()))):
+    print(i)
+
+
+# for i in map(lambda x: x * 2 if x % 2 == 0 else -x , list(map(int, input().split()))):
+#     print(i)
