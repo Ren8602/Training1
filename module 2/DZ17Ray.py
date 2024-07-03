3fb3929a4a59da74d78ef36932e559c85c2d96bb
RayRaf <rayraf@mail.ru>
2024-07-03 16:39:27 +0500

    Добавил DZ16 и DZ17

--- module 2/DZ17Ray.py
+++ module 2/DZ17Ray.py
@@ -1,4 +1,9 @@
-expression = input()
-result = eval(expression)
-print(result)
+def filter(func, seq):
+   for i in seq:
+      if func(i):
+         yield i
 
+func_in, seq_in = eval(input()), eval(input())
+
+for x in filter(func_in, seq_in):
+   print(x)
