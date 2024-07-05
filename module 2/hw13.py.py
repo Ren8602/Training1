db4444196a050f9f14e23ce235a28aa4880c5214
Pear-ui <sarent1m@gmail.com>
2024-07-05 11:48:09 +0500

    HW13

---
+++ module 2/hw13.py.py
@@ -0,0 +1,16 @@
+x = input()
+lines=[]
+while x:= input():
+    if x[0]=='!':
+        x=x.upper()
+        lines.append(x)
+    else:
+        x=x.lower()
+        lines.append(x)
+def func(text):
+    for char in ['!', '@', '#', '%']:
+        text = text.replace(char, '')
+    return text
+# y = func(x)
+for line in lines:
+    print (func(line))
